2d318327391c4e1e4fb6769b73cdc4a4dfbd825d
Mikhail Bakulev <michael.baculev@gmail.com>
2025-06-06 22:53:43 +0400

    fixed: sql script

---
+++ src/main/java/ru/academy/pizza/entity/CustomerEntity.java
@@ -0,0 +1,20 @@
+package ru.academy.pizza.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "customer")
+public class CustomerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_seq")
+    @SequenceGenerator(name = "customer_id_seq", sequenceName = "customer_id_seq", allocationSize = 1)
+    private Long id;
+    private String surname;
+    private String name;
+}
---
+++ src/main/java/ru/academy/pizza/entity/DishEntity.java
@@ -0,0 +1,23 @@
+package ru.academy.pizza.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "dish")
+public class DishEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dish_id_seq")
+    @SequenceGenerator(name = "dish_id_seq", sequenceName = "dish_id_seq", allocationSize = 1)
+    private Long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "kitchen_id")
+    private KitchenEntity kitchen;
+    private double price;
+}
